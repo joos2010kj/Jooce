1ac15109a828aafa4470e7246f704504eb7b1fbd
Hyekang Joo <joos2010kj@github.com>
2022-07-25 16:04:53 -0500

    Change bit

--- __init__.py
+++ __init__.py
@@ -1,2 +1,6 @@
+import inspect
+import json
+from pathlib import Path
+from .utils import history
 from .main import Jooce
 from .transcribe import *
--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
 import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='Jooce',  
+     version='0.0.1',
+     scripts=['Jooce'] ,
+     author="Kevin Hyekang Joo",
+     author_email="joos2010kj@gmail.com",
+     description="Simple and Easy Way to Recycle Boring Codes",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/joos2010kj/Jooce",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
